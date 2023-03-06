70395e42e8bc15a155295961abf262f02d326a83
Ravi <ravkchauhan1aip@gmail.com>
2023-03-06 23:35:25 +0530

    update

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementById('liters')const smallCups = document.querySelectorAll('.cup-small')
 const liters = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
