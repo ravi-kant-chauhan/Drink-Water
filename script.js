4897f1714c7d7295d98e3ab2aadd4f3d8df14849
Ravi <ravkchauhan1aip@gmail.com>
2023-03-06 23:36:12 +0530

    update

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
 updateBigCup()
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx))
+})
